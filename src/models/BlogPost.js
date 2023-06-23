fb0bd7433bd22bc1571f842241c179a6b80d764a
claytongom <claytoncnx@gmail.com>
2023-06-23 07:35:34 -0300

    add novamente BlogPost em PascalCase - MAC deu falso positivo. será isso?

---
+++ src/models/BlogPost.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      userId: DataTypes.INTEGER,
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+      {
+        timestamps: false,
+        tableName: 'blog_posts',
+        underscored: true,
+      });
+    BlogPost.associate = ({ User }) => {
+      BlogPost.belongsTo(User, {
+        foreignKey: 'user_id',
+        as: 'user',
+        targetKey: 'id',
+      });
+    }
+    // correção nome arquivo
+    return BlogPost;
+  };
